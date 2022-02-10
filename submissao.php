d62ea8bc8264f8b2d1f4b9ff15272a4a896d9411
Matheus Serpa <matheus.serpa@digifarmz.com>
2022-02-10 09:16:23 -0300

    adding index

---
+++ submissao.php
@@ -0,0 +1,144 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>WSPPD 2020</title>
+	        <link rel="stylesheet" type="text/css" href="style.css" />
+	        <meta name="google-site-verification" content="m7gydMt8_CjUxyvCzoe7s3Ybgxc4bMqmoU-HhxKrwKY" />
+		</head>
+
+		<body>
+
+<div id="wrapper-menu-top">
+<div id="menu-top">
+&nbsp;
+</div><!--menu-top-->
+</div><!--wrapper-menu-top-->
+<div id="wrapper-header">
+<div id="header">
+<div id="wrapper-header2">
+<div id="wrapper-header3">
+		<h1 align="right"><a href="." style="color: white">WSPPD 2020 - XVIII Workshop de Processamento <br>Paralelo e Distribuído</a></h1>
+		    </div>
+		    </div>
+		    </div>
+		    </div>
+
+
+
+<div id="wrapper-content">
+
+<div id="wrapper-all-menu">
+<table>
+<tr><td>
+<div id="wrapper-menu-page">
+        <div id="menu-page">
+			<h3>Navegação</h3>
+<ul>
+<li><a href=".">Página Inicial</a></li>
+<li><a href="programa.php">Programação</a></li>
+<!--<li><a href="apresentacoes.php">Apresenta&ccedil;&otilde;es <span style="background-color:yellow"><I>( Novo )</I></span></a></li>-->
+<li><a href="topicos.php">Tópicos de Interesse</a></li>
+<li><a href="datas.php">Datas Importantes</a></li>
+<li><a href="foto.php">Fotos do Evento</a></li>
+<!--<li><a href="chamada.php">Chamada de Artigos</a></li>-->
+<!--<li><a href="formato.php">Formato de Submissão</a></li>-->
+<li><a href="submissao.php">Submissão</a></li>
+<li><a href="https://docs.google.com/forms/d/e/1FAIpQLScvSpLoYmXUlukgtyVlYb4uyngfL05BapKGTDZ8Jw27_l2RCA/viewform?usp=sf_link">Inscrições</a></li>
+<li><a href="proceedings.php">Proceedings</a></li>
+<li><a href="comite.php">Comitês</a></li>
+<li><a href="contato.php">Contato</a></li>
+</ul>
+			<br><br>
+	</div><!--menu-page-->
+</div>
+</td></tr>
+<tr><td>
+<div id="wrapper-menu-page2">
+	<div id="menu-page">
+			<h3>Eventos anteriores</h3>
+
+<ul>
+
+<li> <a href="../2019">2019</a> &bull; <a href="../2018">2018</a></li>
+
+<li> <a href="../2017">2017</a> &bull; <a href="../2016">2016</a> &bull; <a href="../2015">2015</a> </li>
+
+<li> <a href="../2014">2014</a> &bull; <a href="../2013">2013</a> &bull; <a href="../2012">2012</a> </li>
+
+<li> <a href="../2011">2011</a> &bull; <a href="../2010">2010</a> &bull; <a href="../2009">2009</a> </li>
+
+<li> <a href="../2008">2008</a> &bull; <a href="../2007">2007</a> &bull; <a href="../2006">2006</a> </li>
+
+<li> <a href="../2005">2005</a> &bull; <a href="../2004">2004</a> &bull; <a href="../2003">2003</a></li>
+
+</ul>
+			<br><br>
+	</div><!--menu-page-->
+</div>
+</td></tr>
+</table>
+</div>
+
+<div id="content">
+
+<h2>Submissão</h2>
+
+<p>
+Os artigos devem ser escritos em <b>inglês</b>, obedecendo as datas
+estipuladas no item <a href="datas.php">Datas Importantes</a>. Cada
+artigo deve conter entre <b>2 a 4 páginas</b>.
+</p>
+
+<p>
+O WSPPD recomenda a submissão de artigos completos, porém alunos de mestrado e 
+doutorado que tenham um artigo publicado neste ano, em seu tópico de pesquisa atual, 
+ou possuem artigo submetido ou pronto para a submissão, podem utilizar a opção de envio 
+<b>Abstract Only</b>. Este formato foi incluído a partir da edição XV do WSPPD para 
+<b>incentivar</b> a <b>colaboração</b> e <b>discussão</b> entre os pesquisadores 
+participantes do evento.
+</p>
+
+<p>
+Pedimos aos autores que usarem a opção Abstract Only que adicionem uma seção de <b>Disclaimer</b>,
+informando em qual conferência ou journal o trabalho foi submetido ou publicado. 
+</p> 
+
+<p>
+O formato LaTeX para submissão está disponível <a
+href="downloads/wsppd-paper.tar.gz">neste pacote</a>. A submissão
+deverá ser feita em PDF (página A4).
+<br>
+Também, está disponível neste <a href="https://github.com/schnorr/ieeeorg">link</a>, o formato LaTeX em org, para quem utiliza org-mode.
+</p>
+
+<p> 
+Todos os autores devem fazer a submissão dos artigos pelo <b>EasyChair</b> através da URL: 
+<center>
+<a href="https://easychair.org/cfp/wsppd2020">https://easychair.org/cfp/wsppd2020</a>
+</center>
+Para a submissão será necessária a criação de uma conta nesse sistema.
+</p>
+
+<p>Os artigos devem estar em formato PDF no momento da submissão.</p>
+
+<p>Todos os artigos aceitos serão publicados nesta página (anais
+eletrônicos) e se possível em outro formato (papel ou mídia).</p>
+
+</div>
+
+<div id="wrapper-footer">
+<div id="footer">
+Design by studio7designs | copyright 2014 <a href="http://www.inf.ufrgs.br/gppd/gppd/">GPPD</a>
+</div>
+</div>
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1585587-5";
+urchinTracker();
+</script>
+</body>
+</html>
